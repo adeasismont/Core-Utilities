7c4974c512b0f6126424e19587fc81310e2db02a
Anton Kiland <anton@kiland.se>
2011-09-09 15:41:20 +0200

    Added a single header file for easy importing.

---
+++ CoreUtilities.h
@@ -0,0 +1,14 @@
+/*
+ Core Utilities
+ 
+ Created by Anton Kiland.
+ 
+ Released as is in the public domain.
+*/
+
+
+#import "NSObject+Core.h"
+#import "NSArray+Core.h"
+#import "NSString+Core.h"
+#import "NSDate+Core.h"
+#import "UIView+Core.h"
